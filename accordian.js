c0955ddfc47c216e6c05eee5ca789878d692aaa5
Asad-Rao <113167578+Asad-Rao@users.noreply.github.com>
2023-02-09 12:31:41 +0500

    Update accordian.js

--- accordian.js
+++ accordian.js
@@ -15,3 +15,11 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+$(document).ready(function () {
+  $(".hamurgermeune").click(function () {
+      $(".bar1").toggleClass("change1")
+      $(".bar2").toggleClass("change2")
+      $(".bar3").toggleClass("change3")
+  });
+});
+
